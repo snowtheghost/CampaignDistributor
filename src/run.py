b14d6a24486c0fc098df9bfe7c31fe800804cb00
Justin Chan <67134961+snowtheghost@users.noreply.github.com>
2021-04-10 22:53:09 -0400

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,5 +1,9 @@
 from flaskapp import app  # from __init__.py
+import os
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
     # app.run(debug=True)  # Debug mode for development
-    app.run(host='192.168.2.10', port=9000)
+
+    load_dotenv()
+    app.run(host=os.getenv("HOST"), port=os.getenv("PORT"))
