c13ead54b72302820f5e9e208ddad2040aeb8203
Justin Chan <67134961+snowtheghost@users.noreply.github.com>
2021-04-10 22:48:22 -0400

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,5 +1,5 @@
 from flaskapp import app  # from __init__.py
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Debug mode for development
-    # app.run()
+    # app.run(debug=True)  # Debug mode for development
+    app.run(host='192.168.2.10', port=9000)
