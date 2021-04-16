e80e1d2129d5b8e0f187d9fa0f99d01448106e59
Justin Chan <67134961+snowtheghost@users.noreply.github.com>
2021-04-16 02:35:53 -0400

    Update run.py

--- src/run.py
+++ src/run.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Debug mode for development
+    # app.run(debug=True)  # Debug mode for development
 
-    # load_dotenv()
-    # app.run(host=os.getenv("HOST"), port=os.getenv("PORT"))
+    load_dotenv()
+    app.run(host=os.getenv("HOST"), port=os.getenv("PORT"))
