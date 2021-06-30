725f8204246041b4004363a6b59fd7391b128984
Justin Chan <csnow.to@gmail.com>
2021-06-30 20:38:10 +0800

    directory restructure

--- run.py
+++ run.py
@@ -3,7 +3,12 @@ import os
 from dotenv import load_dotenv
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Debug mode for development
+    # app.run(debug=True)  # Debug mode for development
 
     # load_dotenv()
     # app.run(host=os.getenv("HOST"), port=os.getenv("PORT"))
+
+    # Heroku Deployment
+    if not os.path.exists('flaskapp/site.db'):
+        exec(open("setup.py").read())
+    app.run()
